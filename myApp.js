6a19a8ccb914cfeab0be0b564913a22da7d44c5b
El-Mahdi Safouane <esafouan@c4r2p1.1337.ma>
2024-01-14 12:02:48 +0100

    node

--- myApp.js
+++ myApp.js
@@ -1,5 +1,11 @@
 let express = require('express');
 let app = express();
+console.log("Hello World");
+function Handler(req, res){
+    const absolutePath = __dirname + '/views/index.html';
+    res.sendFile(absolutePath);
+}
+app.get("/",Handler);
 
 
 
